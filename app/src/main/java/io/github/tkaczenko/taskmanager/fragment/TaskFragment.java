895c64773828bca95644eecf1c3bd96a44f44689
tkaczenko <include.free@gmail.com>
2016-11-27 16:31:19 +0200

    Implemented searching for TaskFragment

--- app/src/main/java/io/github/tkaczenko/taskmanager/fragment/TaskFragment.java
+++ app/src/main/java/io/github/tkaczenko/taskmanager/fragment/TaskFragment.java
@@ -8,12 +8,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.github.tkaczenko.taskmanager.R;
@@ -27,17 +31,71 @@ import io.github.tkaczenko.taskmanager.database.repository.TaskDAO;
 
 public class TaskFragment extends Fragment {
     private RecyclerView mRecyclerView;
-
+    private TaskAdapter mAdapter;
     private Activity activity;
+    private GetTasksTask task;
+    private OnTaskSelectedListener mListener;
 
     private TaskDAO taskDAO;
-
-    private GetTasksTask task;
-
-    private OnTaskSelectedListener mListener;
+    private List<Task> mList;
 
     public interface OnTaskSelectedListener {
         void onTaskSelected(Task task);
+    }
+
+    private SearchView.OnQueryTextListener mSearchListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            newText = newText.toLowerCase();
+            List<Task> filteredList = new ArrayList<>();
+            for (int i = 0; i < mList.size(); i++) {
+                String taskSource = mList.get(i).getTaskSource().getName();
+                String taskType = mList.get(i).getTaskType().getName();
+                String shortName = mList.get(i).getShortName();
+                String description = mList.get(i).getDescription();
+                String rejectionReason = mList.get(i).getRejectionReason();
+                String sourceDoc = mList.get(i).getSourceDoc();
+                String sourceNum = mList.get(i).getSourceNum();
+                if (taskSource != null && taskSource.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (taskType != null && taskType.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (shortName != null && shortName.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (sourceDoc != null && sourceDoc.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (sourceNum != null && sourceNum.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (description != null && description.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                } else if (rejectionReason != null && rejectionReason.toLowerCase().contains(newText)) {
+                    filteredList.add(mList.get(i));
+                }
+            }
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new TaskAdapter(filteredList,
+                    new TaskAdapter.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Task task) {
+                            mListener.onTaskSelected(task);
+                        }
+                    });
+            mRecyclerView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+            return true;
+        }
+    };
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(mSearchListener);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -45,6 +103,7 @@ public class TaskFragment extends Fragment {
         super.onCreate(savedInstanceState);
         activity = getActivity();
         taskDAO = new TaskDAO(activity);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -91,16 +150,17 @@ public class TaskFragment extends Fragment {
         protected void onPostExecute(List<Task> tasks) {
             if (activityWeakRef.get() != null
                     && !activityWeakRef.get().isFinishing()) {
+                mList = tasks;
                 if (tasks != null) {
                     if (tasks.size() != 0) {
-                        TaskAdapter adapter = new TaskAdapter(tasks,
+                        mAdapter = new TaskAdapter(tasks,
                                 new TaskAdapter.OnItemClickListener() {
                                     @Override
                                     public void onItemClick(Task task) {
                                         mListener.onTaskSelected(task);
                                     }
                                 });
-                        mRecyclerView.setAdapter(adapter);
+                        mRecyclerView.setAdapter(mAdapter);
                     } else {
                         Toast.makeText(activity, "No Tasks Records",
                                 Toast.LENGTH_LONG).show();
